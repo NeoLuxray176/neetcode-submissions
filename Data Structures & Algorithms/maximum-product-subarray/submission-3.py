b341d34599059ef90147c415d7ad96161ea0344b
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-07-16 15:44:44 +0200

    Add: maximum-product-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        # Brute Force: check every sub array
+
+        n = len(nums)
+
+        res = min(nums)
+
+        for i in range(n):
+            tmp =  nums[i]
+            for j in range(i+1,n):
+                tmp *= nums[j]
+                res = max(tmp, res)
+
+        return res
