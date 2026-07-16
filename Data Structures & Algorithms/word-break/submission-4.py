55a796b560f2aabb86b2a24dab55d5a9a24027a4
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-07-16 16:06:14 +0200

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    cache = {}
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # Greedy does not work because we could use a word that prevents a solution
+
+        # Decision tree with one child per word in the dict
+        # Use that word or use something else
+
+        if not s:
+            return True
+
+        if s in self.cache:
+            return self.cache[s]
+
+        res = False
+        for word in wordDict:
+            if s.startswith(word):
+                # print(f"In {s} remove {word} checking {s[len(word):]}")
+                res = res or self.wordBreak(s[len(word):], wordDict)
+
+                if res:
+                    self.cache[s] = True
+
+        return res
