15032584f724a8bd9e6f205c000b16ba94e0a197
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-07-18 18:36:27 +0200

    Add: climbing-stairs - submission-11

---
+++ Data Structures & Algorithms/climbing-stairs/submission-11.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # For each step, calculate the number of ways that we can reach it
+        # For the first step this is one way
+        # 2. two ways
+        # 3. three ways (1 + 2, 2 + 1, 1 + 1 + 1)
+
+        dp = [0] * (n)
+
+        dp[0] = 1
+        dp[1] = 2
+
+        for i in range(2, n):
+            dp[i] = dp[i - 2] + dp[i - 1]
+
+        return dp[-1]
