746443db7e14c3d0e20ad8d81f453f7159588691
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-08-19 16:27:05 +0200

    Add: edit-distance - submission-1

---
+++ Data Structures & Algorithms/edit-distance/submission-1.py
@@ -0,0 +1,27 @@
+from functools import cache
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+
+        @cache
+        def dfs(i: int, j: int) -> int:
+            # All remaining charactesr in word2 must be inserted
+            if i == m:
+                return n - j
+            # All remaining charactesr in word1 must be inserted
+            if j == n:
+                return m - i
+
+            # No operation needed, continue
+            if word1[i] == word2[j]:
+                return dfs(i + 1, j + 1)
+
+            # Consider all three operations. Delete, insert and replace and
+            # take the min of all three.
+            # Add one for the current operation (whatever it was)
+            res = min(dfs(i + 1, j), dfs(i, j + 1))
+            res = min(res, dfs(i + 1, j + 1))
+            return res + 1
+
+        return dfs(0, 0)
