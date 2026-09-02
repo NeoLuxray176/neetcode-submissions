3168ca073b63183bae0d5d6ca8093765294a0f3a
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-09-02 19:16:27 +0200

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Use a dictionary
+        n = len(nums)
+        rems = {}
+
+        for i in range(n):
+            if nums[i] in rems:
+                # print(f"Found solution {i} {rems[nums[i]]}")
+                a, b = i, rems[nums[i]][0]
+                if a < b:
+                    return [a, b]
+                else:
+                    return [b, a]
+            
+            rem = target - nums[i]
+            if rem not in rems:
+                rems[rem] = [i]
+            else:
+                rems[rem].append(i)
+
+        return [-1, -1]
