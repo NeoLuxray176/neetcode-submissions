53056e33f6972e94f6ef4679fa7fb5a4872b66f4
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-09-05 08:58:56 +0200

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Go through the string and for every open bracket we encounter
+        # add the corresponding close bracket to the stack.
+        # If we encounter a close bracket in the string, check whether it matches what is on
+        # top of the stack. If it is we can continue with the next character in the string
+        # otherwise the string is not valid.
+
+        stack = []
+
+        for cha in s:
+            if cha == "(":
+                stack.append(")")
+            if cha == "[":
+                stack.append("]")
+            if cha == "{":
+                stack.append("}")
+            
+            if cha == ")" or cha == "]" or cha == "}":
+                if stack and stack[-1] == cha:
+                    stack.pop()
+                else:
+                    return False
+
+        return True
