692c8ab61df075af637e93d7990b298c0e4095c2
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-09-05 09:24:22 +0200

    Add: evaluate-reverse-polish-notation - submission-8

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-8.py
@@ -0,0 +1,28 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        # Evaluate each expression as we encounter it
+        # We can use a stack to parse the tokens if we encounter a number we parse it to int and
+        # put it on top of the stack.
+        # If we encounter an operator, we pop the two operands off of the stack we can then apply the operator and add the result back to the stack
+
+        stack = []
+
+        for token in tokens:
+            # print(stack)
+            if token == "+":
+                a, b = stack.pop(), stack.pop()
+                stack.append(a + b)
+            elif token == "-":
+                b, a = stack.pop(), stack.pop()
+                # print(f"{a} - {b}")
+                stack.append(a - b)
+            elif token == "*":
+                a, b = stack.pop(), stack.pop()
+                stack.append(a * b)
+            elif token == "/":
+                b, a = stack.pop(), stack.pop()
+                stack.append(a // b)
+            else:
+                stack.append(int(token))
+
+        return stack[-1]
