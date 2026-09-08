fe4ec69bfcdb31fceabf1305c13359c811195926
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-09-08 19:53:18 +0200

    Add: sliding-window-maximum - submission-2

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        heap = []
+        res = []
+
+        for i in range(len(nums)):
+            # Push the items onto the heap (remember this is a min heap so we put the negative 
+            # of the actual value here)
+            heapq.heappush(heap, (-nums[i], i))
+            if i + 1 >= k:
+                # pop items until we have reached the largest element in the current interval
+                while heap[0][1] <= i - k:
+                    heapq.heappop(heap)
+                # The largest element in the current interval
+                res.append(-heap[0][0])
+        
+        return res
