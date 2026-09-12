049370b510a71b64a07b37d4088f6315bc2f8bba
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-09-12 10:26:40 +0200

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        n, m = len(board), len(board[0])
+        directions = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+
+        def dfs(k : int, i : int, j : int) -> bool:
+            if k == len(word):
+                return True
+            if i < 0 or i >= n:
+                return False
+            if j < 0 or j >= m:
+                return False
+            if board[i][j] != word[k]:
+                return False
+
+            for a, b in directions:
+                if dfs(k + 1, i + a, j + b):
+                    return True
+            return False
+
+        for i in range(n):
+            for j in range(m):
+                if dfs(0, i, j):
+                    return True
+        return False
