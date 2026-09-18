f554935a82687c28dc28a2c29986b4bb134dd77a
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-09-18 11:02:26 +0200

    Add: fruit-into-baskets - submission-3

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        # Task
+        # Find the type that occurs the most and second most in the array
+
+        # General idea
+        # Starting from every tree we need to pick every fruit
+        # So we want to find the longest sequence consisting of only one of two numbers starting from an index
+
+        n = len(fruits)
+        res, curr_res = 0, 0
+        curr_set = set()
+
+        for i in range(n):
+            curr_res = 0
+            curr_set = set()
+            for j in range(i, n):
+                if len(curr_set) < 2:
+                    curr_set.add(fruits[j])
+                    curr_res += 1
+                elif fruits[j] in curr_set:
+                    curr_res += 1
+                else:
+                    break
+                
+                res = max(res, curr_res)
+
+        return res
+
+
+        
+        
