3420da624b1303c4256bc58ce92346254d90274c
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-09-19 21:14:44 +0200

    Add: network-delay-time - submission-7

---
+++ Data Structures & Algorithms/network-delay-time/submission-7.py
@@ -0,0 +1,27 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # Bellman-Ford
+        # General Idea
+        # We set up an array tracking the distances with initial value infinity
+        # We set the source's distance to zero.
+        # Then we iterate over all edges V - 1 times and try to find new shortest pahts
+        # from now potentially reachable nodes. Note that initially only the source is reachable
+        # In the end we will have an array of shortest distances.
+
+        dists = [float("inf")] * (n + 1)
+        dists[k] = 0
+
+        for _ in range(n):
+            for u, v, weight in times:
+                changed = False
+                if dists[u] + weight < dists[v]:
+                    dists[v] = dists[u] + weight
+                    changed = True
+
+            if not changed:
+                break
+
+        if max(dists[1:]) == float("inf"):
+            return -1
+
+        return max(dists[1:])
