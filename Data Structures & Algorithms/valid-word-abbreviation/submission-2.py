0997a1a0c5e19645cc95edf02ed5671675d9c6d1
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-09-21 20:39:45 +0200

    Add: valid-word-abbreviation - submission-2

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        # word="apple"
+        # abbr="a4"
+        # expect true
+
+        i = 0
+        curr_digit = ""
+        while i < len(abbr):
+            # print(abbr[i], abbr[i].isdigit())
+            if abbr[i].isdigit():
+                if abbr[i] == "0":
+                    return False
+                curr_digit += abbr[i]
+                i += 1
+            elif curr_digit:
+                i += int(curr_digit)
+                curr_digit = ""
+            else:
+                if word[i] != abbr[i]:
+                    return False
+                i += 1
+        
+        if curr_digit:
+            print("hello abc", i + int(curr_digit), len(word))
+            return i + int(curr_digit) == len(word)
+
+        # print(i, len(word))
+        return i == len(word)
+                
