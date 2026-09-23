6bda0e0f68db6a7a35960bc9c23437b317e5c96e
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-09-23 14:45:59 +0200

    Add: car-fleet - submission-22

---
+++ Data Structures & Algorithms/car-fleet/submission-22.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = list(zip(position, speed))
+
+        stack = []
+
+        for pos, spd in cars:
+            arrival_time = (target - pos) / spd
+            stack.append(arrival_time)
+            
+            while len(stack) >= 2 and stack[-2] >= stack[-1]:
+                stack.pop()
+
+        return len(stack)
