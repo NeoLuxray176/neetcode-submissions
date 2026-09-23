8cd9297c361f5ce177d0f5d681e15d211fa45551
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-09-23 09:28:52 +0200

    Add: minimum-number-of-operations-to-move-all-balls-to-each-box - submission-0

---
+++ Data Structures & Algorithms/minimum-number-of-operations-to-move-all-balls-to-each-box/submission-0.py
@@ -0,0 +1,71 @@
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        # Task
+        # Given a list of boxes of length n.
+        # Calculate the number of operations we have to do to move all balls to the i-th box. For all i in [0,n].
+        # An operation constitutes the move of one ball to an adjacent box.
+        # So moving two balls into an adjacent box takes two operations.
+        # Moving one ball into an adjacent box takes two operations. Moving it two boxes takes two operations.
+
+        # Constraints:
+        # We may have zero boxes, in that case it takes no operations
+        
+        # General idea:
+        # Brute force approach:
+        # For each index, calculate the result explicitly by computing the distance of every other ball to our box.
+        # So when calculating the operations for index 5, a ball at index 2. Would have to be moved to index 3, 4, and 5. Which
+        # is 5 - 2 = 3 operations.
+        # For every index we have to compute the operations required for all other indices. This is n * (n-1) so in O(n^2)
+
+        # Can we do better?
+        # Given a prefix operations count and a suffix operations count we can compute the number of operations as
+        # 2 * (poc[i - 1] + soc[i + 1]).
+        # Now easier we could just combine poc[i] and soc[i] because at that point the number of operations to move all balls to the left of
+        # us is stored at poc[i].
+
+        # Can we do even better?
+        # No, because we do need information about the whole left and right side.
+        # So this would run in linear time and uses linear space as well.
+
+        # Tests
+        # "110"
+        # [1, 1, 3]
+
+        boxes = list(boxes) # "110" -> ["1", "1", "0"]
+        n = len(boxes) # 3
+        for i in range(n): # [1, 1, 0]
+            boxes[i] = int(boxes[i])
+
+        prefix_oc = [0] * n
+        suffix_oc = [0] * n
+        prefix_sum = [0] * n
+        suffix_sum = [0] * n
+
+        for i in range(1, n):
+            prefix_sum[i] = prefix_sum[i - 1] + boxes[i - 1]
+        # [1, 2, 0]
+        print("prefix_sum", prefix_sum)
+        for i in range(n - 2, -1, -1):
+            suffix_sum[i] = suffix_sum[i + 1] + boxes[i + 1]
+        # [2, 1, 0]
+        print("suffix_sum", suffix_sum)
+
+        for i in range(1, n):
+            prefix_oc[i] = prefix_oc[i - 1] + prefix_sum[i]
+            # [0, 0, 0]
+            # i = 1; [0, 1, 0]
+            # i = 2; [0, 1, 3]
+        print("prefix_oc", prefix_oc)
+
+        for i in range((n - 1) - 1, -1, -1):
+            suffix_oc[i] = suffix_oc[i + 1] + suffix_sum[i]
+            # [0, 0, 0]
+            # i = 1; [0, 0, 0]
+            # i = 1; [1, 0, 0]
+        print("suffix_oc", suffix_oc)
+
+        res = [0] * n
+        for i in range(n):
+            res[i] = prefix_oc[i] + suffix_oc[i]
+
+        return res
