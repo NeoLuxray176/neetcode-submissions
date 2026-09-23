84d7286e8de69ffd7d914b0b37e8a7c88e2e73a1
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-09-23 17:33:51 +0200

    Add: gas-station - submission-2

---
+++ Data Structures & Algorithms/gas-station/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # General Idea
+        #  Since we can choose the starting point, we can complete the circuit as long as the remaining gas after one round is positive
+        
+        start_idx = 0
+        tank = total = 0
+
+        for i in range(len(gas)):
+            balance = gas[i] - cost[i]
+
+            total += balance
+            tank += balance
+
+            if tank < 0:
+                start_idx = i + 1
+                tank = 0
+
+        return start_idx if total >= 0 else -1
+
+        # total = tank = start = 0
+        # for i, (fuel, expense) in enumerate(zip(gas, cost)):
+        #     balance = fuel - expense
+        #     total += balance
+        #     tank += balance
+        #     if tank < 0:
+        #         start = i + 1
+        #         tank = 0
+        # return start if total >= 0 else -1
