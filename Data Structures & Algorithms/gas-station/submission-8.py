46767be373319fcfb46c9f75270f432ae2a7f924
NeoLuxray176 <300056489+NeoLuxray176@users.noreply.github.com>
2026-09-23 18:30:32 +0200

    Add: gas-station - submission-8

---
+++ Data Structures & Algorithms/gas-station/submission-8.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+
+        start = 0
+        tank = total = 0
+
+        for i, (incoming, outgoing) in enumerate(zip(gas, cost)):
+            balance = incoming - outgoing
+            tank += balance
+            total += balance
+
+            if tank < 0:
+                start = i + 1
+                tank = 0
+
+        return start if total >= 0 else -1
